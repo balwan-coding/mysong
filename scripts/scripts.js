05739f14573c0759ef0d0758426c65df846c3f97
balwan coding <balwansinghrajput1708@gmail.com>
2025-01-11 13:56:25 +0530

    remove

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -131,17 +131,4 @@ async function main() {
     });
 }
 
-//  document
-//    .querySelector(".range")
-//    .getElementsByTagName("input")[0]
-//    .addEventListener("change", (e) => {
-//      console.log("Setting volume to", e.target.value, "/ 100");
-//      currentSong.volume = parseInt(e.target.value) / 100;
-//      if (currentSong.volume > 0) {
-//        document.querySelector(".volume>img").src = document
-//          .querySelector(".volume>img")
-//          .src.replace("mute.svg", "volume.svg");
-//      }
-//    });
-
 main();
